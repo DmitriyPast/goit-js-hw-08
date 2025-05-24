6b8b6891504aa215d2f3125a1452b90f3b149ccf
your_name <pastushenkodmitry2003@gmail.com>
2025-05-24 13:08:15 +0300

    hw8.js first attempt

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const elements = document.querySelectorAll(".item");
-console.log(`Number of categories: ${elements.length}`);
-// console.log(elements[0].children[0].textContent);
-// console.log(elements[0].children[1].children.length);
-elements.forEach(element => {
-    console.log(`Category: ${element.children[0].textContent}`);
-    console.log(`Elements: ${element.children[1].children.length}`);
-})
